ff4277feef2b476c57cc2bff38e0ddb9ca74c499
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-11-02 02:09:40 +0300

    heap sort implementation and tests

---
+++ src/heap_sort.py
@@ -0,0 +1,27 @@
+# Heap sort algorithm
+def heap_sort(arr: list) -> list:
+    # Makes max-heap
+    def heapify(n: int, i: int) -> None:
+        largest = i
+        left = i * 2 + 1
+        right = i * 2 + 2
+
+        if left < n and arr[largest] < arr[left]:
+            largest = left
+        if right < n and arr[largest] < arr[right]:
+            largest = right
+
+        if largest != i:
+            arr[largest], arr[i] = arr[i], arr[largest]
+            heapify(n, largest)
+
+    n = len(arr)
+    # Make max-heap from the given array
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(n, i)
+
+    # Sort array by placing max element in the heap in the correct place
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(i, 0)
+    return arr
---
+++ test/heap_sort_test.py
@@ -0,0 +1,17 @@
+from src.heap_sort import heap_sort
+import pytest
+
+
+@pytest.mark.parametrize(
+    ["input", "output"],
+    [
+        ([4, 3, 2, 0, -1, 4, 3], [-1, 0, 2, 3, 3, 4, 4]),
+        ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+        (
+            [5, 4, 1, 13, 345, 1, 403, -14, -5, 1],
+            [-14, -5, 1, 1, 1, 4, 5, 13, 345, 403],
+        ),
+    ],
+)
+def test_heap_sort(input, output):
+    assert heap_sort(input) == output
