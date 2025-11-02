1108908e11fe0cce7893bcd3d1a20801350a447e
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2025-11-02 07:58:14 +0300

    quick sort implementation and tests

---
+++ src/quick_sort.py
@@ -0,0 +1,25 @@
+def quick_sort(array: list) -> list:
+    if len(array) <= 1:
+        return array
+
+    def partition(array: list):
+        """
+        Take pivot element and partition the list,
+        Return array and position of the element
+        that is on the right position.
+        """
+        pivot = array[-1]
+        result = 0
+        for i in range(len(array) - 1):
+            if array[i] < pivot:
+                array[i], array[result] = array[result], array[i]
+                result += 1
+
+        array[-1], array[result] = array[result], array[-1]
+        return array, result
+
+    array, pivot = partition(array)
+    # Sort other parts of the array
+    array[:pivot] = quick_sort(array[:pivot])
+    array[pivot + 1 :] = quick_sort(array[pivot + 1 :])
+    return array
---
+++ test/quick_sort_test.py
@@ -0,0 +1,17 @@
+from src.quick_sort import quick_sort
+import pytest
+
+
+@pytest.mark.parametrize(
+    ["input", "output"],
+    [
+        ([4, 3, 2, 0, -1, 4, 3], [-1, 0, 2, 3, 3, 4, 4]),
+        ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+        (
+            [5, 4, 1, 13, 345, 1, 403, -14, -5, 1],
+            [-14, -5, 1, 1, 1, 4, 5, 13, 345, 403],
+        ),
+    ],
+)
+def test_quick_sort(input, output):
+    assert quick_sort(input) == output
